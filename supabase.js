efbc388da224e5f594f89d3cef6b8c9bf9975060
Lor123489 <147886195+Lor123489@users.noreply.github.com>
2026-09-23 15:52:31 +0300

    Initialize Supabase client with URL and key

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://jihhlgwrpqwioxrpgthf.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_m0ZGjTZX8T1gfCN4CNV3QA_s1BC667J";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
